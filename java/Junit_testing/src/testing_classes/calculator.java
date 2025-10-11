7ea6cb5965ad1e52a317e0c61312e36d7b080676
Anurag Nayak <anu870906@gmail.com>
2025-10-11 20:55:13 +0530

    Delete java directory

--- java/Junit_testing/src/testing_classes/calculator.java
+++
@@ -1,9 +0,0 @@
-package testing_classes;
-public class calculator{
-    public int add(int a, int b){
-        return a+b;
-    }
-    public int multiply(int a, int b){
-        return a*b;
-    }
-}
